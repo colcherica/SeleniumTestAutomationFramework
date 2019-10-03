dd7f4de7023535fefd4deef2f5f05449671c3661
casian <casiancolcher@gmail.com>
2019-10-04 00:50:16 +0300

    adding page constructor

--- src/test/java/com/herokuapp/webpages/HomePage.java
+++ src/test/java/com/herokuapp/webpages/HomePage.java
@@ -1,6 +1,17 @@
 package com.herokuapp.webpages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
 public class HomePage {
+
+    private WebDriver driver;
+
+    public HomePage ( WebDriver driver ) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
 
 
 }
