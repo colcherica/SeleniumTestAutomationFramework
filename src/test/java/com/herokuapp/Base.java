873c9ee508a7ec5545306269e49b0eb555b835bf
colcherc <casiancolcher@gmail.com>
2019-10-04 17:22:14 +0300

    add teardown and refactor the initialization into setUp

--- src/test/java/com/herokuapp/Base.java
+++ src/test/java/com/herokuapp/Base.java
@@ -7,22 +7,25 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.concurrent.TimeUnit;
 
 public class Base {
+
 public static WebDriver driver;
 
-
     @BeforeAll
-    public static void initialize() {
+    public static void setUp() {
+
         System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
         driver = new ChromeDriver();
         String URL = "http://the-internet.herokuapp.com/login";
         driver.get(URL);
+
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
     }
 
     @AfterAll
     public static void tearDown(){
-        driver.quit();
-
+        if (driver != null){
+            driver.quit();
+        }
     }
 }
