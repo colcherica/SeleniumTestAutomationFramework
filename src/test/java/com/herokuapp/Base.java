db31d4dda9148fcbfba864aec590c81e91ceb3b7
colcherc <casiancolcher@gmail.com>
2019-10-04 14:09:02 +0300

    take out the start/end time

--- src/test/java/com/herokuapp/Base.java
+++ src/test/java/com/herokuapp/Base.java
@@ -4,19 +4,15 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import java.util.concurrent.TimeUnit;
 
 public class Base {
 public static WebDriver driver;
 
-static long startTime = System.currentTimeMillis();
-static long endTime = System.currentTimeMillis();
 
     @BeforeAll
     public static void initialize() {
         System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
-        System.out.print("Run started at time " + startTime + "\n");
         driver = new ChromeDriver();
         String URL = "http://the-internet.herokuapp.com/login";
         driver.get(URL);
@@ -26,7 +22,7 @@ static long endTime = System.currentTimeMillis();
 
     @AfterAll
     public static void tearDown(){
-        System.out.println("Run ended at time " + endTime + "\n");
         driver.quit();
+
     }
 }
