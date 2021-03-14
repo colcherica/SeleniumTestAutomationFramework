51a44ee6308465a1b9d01434b1165f522090fa16
Casian Colcher <casiancolcher@gmail.com>
2021-03-14 15:21:17 +0200

    deleted unused test

--- src/test/java/com/herokuapp/tests/MainPage_Test.java
+++ src/test/java/com/herokuapp/tests/MainPage_Test.java
@@ -16,15 +16,4 @@ public class MainPage_Test extends Base {
         Assert.assertFalse(mainPage.allLinksDisplayed(), "All links from " + MainPage.class.getName() +
                 " are displayed on the page");
     }
-
-    @Test
-    public void gotoAB_Page() {
-        MainPage mainPage = new MainPage(driver);
-        mainPage.click_on_ABPage();
-        A_B_TestingPage a_b_testingPage = new A_B_TestingPage(driver);
-        Assert.assertEquals(a_b_testingPage.getPagedescription(),"Also known as split testing. This is a way" +
-                " in which businesses are able to simultaneously test and learn different versions of a page to see" +
-                " which text and/or functionality works best towards a desired outcome (e.g. a user action such as a" + "" +
-                " click-through).");
-    }
 }
